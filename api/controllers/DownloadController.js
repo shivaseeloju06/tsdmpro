62078edecb210ff9c25591db5aaaa66f96c2e71c
veraw <Wayne.Vera@sqs.com>
2021-03-16 16:30:07 +0200

    Dummy implementation for Download Route and Controllers

--- api/controllers/DownloadController.js
+++ api/controllers/DownloadController.js
@@ -4,27 +4,68 @@ var mongoose = require('mongoose');
 var path = require('path');
 var mime = require('mime');
 var fs = require('fs');
+const { execSync } = require("child_process");
 
-exports.downloadExcelDummy = function (req, res) {
-  res.setHeader(
-    "Content-Type",
-    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-  );
-  res.setHeader(
-    "Content-Disposition",
-    "attachment; filename=" + "Dummy.xlsx"
-  );
+var Project = mongoose.model('Project'),
+  Testsuite = mongoose.model('Testsuite');
 
-  var file = __dirname + '../../../test.xlsx';
+exports.downloadExcelDummy = async function (req, res) {
 
-  var filename = path.basename(file);
-  var mimetype = mime.lookup(file);
+  var file = __dirname + '../../../bin/TSDM.Excel.Exporter.exe';
+  var output = __dirname + '../../../bin/TSDM.xlsx';
+  var jsonFile = __dirname + '../../../bin/TSDM.json';
 
-  res.setHeader('Content-disposition', 'attachment; filename=' + filename);
+  var projectID = req.query.projectID || "6049f040ada0794850f0deca";
+
+  var test = execSync(`TSDM.Excel.Exporter.exe "Test.json" "TSDM.xlsx"`, { cwd: path.dirname(file) });
+  console.log(test.toString());
+
+  var outputname = path.basename(output);
+  var outputFile = path.normalize(output);
+  var mimetype = mime.lookup(output);
+
+  res.setHeader('Content-disposition', 'attachment; filename=' + outputname);
   res.setHeader('Content-type', mimetype);
 
-  var filestream = fs.createReadStream(file);
+  var filestream = fs.createReadStream(output);
   filestream.pipe(res);
+
+  // Testsuite.find({ project: projectID }, (err, testSuites) => {
+  //   var items = [];
+
+  //   testSuites.forEach(ts => {
+  //     items.push({
+  //       "ID": ts.alm_id,
+  //       "ParentID": null,
+  //       "Data": {
+  //         "Work Item Type": "User Story",
+  //         "Title": ts.name
+  //       },
+  //       "Children": []
+  //     })
+  //   });
+
+  //   var jsonContent = JSON.stringify(items);
+  //   console.log(jsonContent);
+
+  //   fs.writeFileSync(jsonFile, jsonContent, 'utf8');
+
+
+  //   var test = execSync(`TSDM.Excel.Exporter.exe "TSDM.json" "TSDM.xlsx"`, { cwd: path.dirname(file) });
+  //   console.log(test.toString());
+
+  //   var outputname = path.basename(output);
+  //   var outputFile = path.normalize(output);
+  //   var mimetype = mime.lookup(output);
+
+  //   res.setHeader('Content-disposition', 'attachment; filename=' + outputname);
+  //   res.setHeader('Content-type', mimetype);
+
+  //   var filestream = fs.createReadStream(output);
+  //   filestream.pipe(res);
+  // });
+
+
   //res.send(200);
 };
 
