cdb42df17fc17d8389599c83445938b9b4c66ab5
riaanroos <riaan.roos@expleogroup.com>
2021-03-17 07:43:11 +0200

    fixed adding and removing step actions to collection

--- api/controllers/StepactionController.js
+++ api/controllers/StepactionController.js
@@ -92,42 +92,78 @@ exports.create_a_stepaction = function (req, res) {
 };
 
 exports.create_a_step_by_stepaction = async function (req, res) {
-  await Stepaction.findById(req.params.id, async function (err, stepaction) {
-    var action_array = req.body;
-    var wip_action_collection = [];
-    await action_array.forEach(async (element) => {
-      if (element.action_id == null) {
-        var new_action_array = {
-          "description": element.description, 
-          "expected_result": element.expected_result,
-          "instruction": element.instructionID,
-          "test_data": element.test_data
-        };
-        var new_action = new Action(new_action_array);
-        await new_action.save().exec(function (err, action) {
-          if (err) {
-            res.send(err);
-            console.log(err);
-            return;
-          };
-          //res.json(action);
-        }); 
-        wip_action_collection.push({"index": element.index, "action": new_action._id});
-      } else {
-        var thisaction = await Action.findById(element.action_id).exec();
-        wip_action_collection.push({"index": element.index, "action": thisaction._id});
-      }
-    });
-
+  let wip_action_collection = await buildCollection(req.body);
+  var thisStepAction = await Stepaction.findById(req.params.id, async function (err, stepaction) {
     stepaction.wip_step_collection = wip_action_collection;
-    stepaction.save((e, updated) => {
+    await stepaction.save((e, updated) => {
       if (e) {
         //res.send(e);
         console.log(e);
         return;
       };
+      return updated;
     });
+    let step_collection = await getStepCollection(stepaction);
+    //console.log(step_collection);
+    return res.json(step_collection);
+    });
+};
 
-    return res.json(stepaction.wip_step_collection);
+async function buildCollection(coll) {
+  var return_collection = [];
+  coll.forEach(async (element) => {
+    if (element.action_id == null) {
+      var new_action_array = {
+        "description": element.description, 
+        "expected_result": element.expected_result,
+        "instruction": element.instructionID,
+        "test_data": element.test_data
+      };
+      let new_action = await createNewAction(new_action_array);
+      //console.log(new_action);
+      return_collection.push({"index": element.index, "action": new_action._id});
+    } else {
+      let thisaction = await Action.findById(element.action_id).exec();
+      return_collection.push({"index": element.index, "action": thisaction._id});
+    }
   });
+  return return_collection;
+};
+
+async function createNewAction(rec) {
+  var query = {"description": rec.description},
+  options = { upsert: true, new: true, setDefaultsOnInsert: true };
+
+  var new_action = Action.findOneAndUpdate(query, rec, options, function(error, result) {
+    if (error) {
+      console.log(error);
+      return;
+    };
+  });
+  return new_action;
+};
+
+async function getStepCollection(request) {
+  var returnArray = [];
+  var thisStepAction = await Stepaction.findById(request._id)
+    .populate({path: 'wip_step_collection.action', model: 'Action'}).exec();
+  var wip_step_collection = thisStepAction.wip_step_collection;
+  await wip_step_collection.forEach( async (element) => {
+    try {
+      var line = {
+        "action_id": element.action._id,
+        "index": element.index,
+        "description": element.action.description,
+        "expected_result": element.action.expected_result,
+        "instruction": element.action.instruction,
+        "test_data": element.action.test_data
+      };
+      returnArray.push(line);
+    } catch (error) {
+      console.log(error);
+      return error;
+    }
+  });
+  console.log(returnArray);
+  return returnArray;
 };
