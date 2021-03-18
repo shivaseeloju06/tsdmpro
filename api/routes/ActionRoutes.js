42f1bc935d3abc09b8f2264acab32f4651a04bbc
veraw <Wayne.Vera@sqs.com>
2021-03-18 21:50:58 +0200

    Add read instruction by id

--- api/routes/ActionRoutes.js
+++ api/routes/ActionRoutes.js
@@ -1,5 +1,5 @@
 'use strict';
-module.exports = function(app) {
+module.exports = function (app) {
   var Action = require('../controllers/ActionController');
 
   // TSDM Routes
@@ -8,11 +8,11 @@ module.exports = function(app) {
     .get(Action.list_all_actions)
     .post(Action.create_an_action);
 
-    app.route('/action/:id')
+  app.route('/action/:id')
     .get(Action.read_an_action_by_id)
     .put(Action.update_an_action_by_id)
     .delete(Action.delete_an_action_by_id);
 
-    app.route('/action/search/:description')
+  app.route('/action/search/:description')
     .get(Action.list_all_actions_by_wildcard);
 };
