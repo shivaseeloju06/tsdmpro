00c04474c51e3c2136c3ba301912b69519e5297a
riaanroos <riaan.roos@expleogroup.com>
2021-03-18 16:26:07 +0200

    create dataiteraatuin and children on create data iteration

--- api/controllers/DataiterationController.js
+++ api/controllers/DataiterationController.js
@@ -1,6 +1,8 @@
 'use strict';
 var mongoose = require('mongoose'),
-  Dataiteration = mongoose.model('Dataiteration');
+  Dataiteration = mongoose.model('Dataiteration'),
+  Tokenname = mongoose.model('Tokenname'),
+  Keyvaluepair = mongoose.model('Keyvaluepair');
 
 exports.list_all_dataiterations = function (req, res) {
   Dataiteration.find({}, function (err, dataiteration) {
@@ -14,7 +16,10 @@ exports.list_all_dataiterations = function (req, res) {
 };
 
 exports.create_a_dataiteration = async function (req, res) {
-  var new_dataiteration = new Dataiteration(req.body);
+  var new_record = req.body;
+  var keyvaluepairs = await fillIterationWithExistingTokens();
+  new_record.keyvaluepairs = keyvaluepairs;
+  var new_dataiteration = new Dataiteration(new_record);
   new_dataiteration.save(function (err, dataiteration) {
     if (err) {
       res.send(err);
@@ -55,6 +60,58 @@ exports.delete_a_dataiteration_by_id = function (req, res) {
       return;
     };
     res.json({ message: 'Dataiteration successfully deleted' });
+  });
+};
+
+exports.list_all_dataiterations_for_stepactions = function (req, res) {
+  Dataiteration.find({}, function (err, dataiteration) {
+    if (err) {
+      res.send(err);
+      console.log(err);
+      return;
+    };
+    res.json(dataiteration);
+  });
+};
+
+
+// TODO MOve business logic into seperate controller
+
+async function fillIterationWithExistingTokens() {
+  return new Promise( async function (resolve, reject) {
+    var returnCollection = [];
+    var allTokens = await getallTokens();
+    await allTokens.forEach(async function (element) {
+      var thisKeyvaluepair = await createEmptyKeyvaluepair(element);
+      returnCollection.push(thisKeyvaluepair._id);
+    });
+    console.log(returnCollection);
+    resolve(returnCollection);
+  });
+};
+
+async function createEmptyKeyvaluepair(token){
+  return new Promise( async function (resolve, reject) {
+    var new_keyvaluepair = new Keyvaluepair({"token_name": token.name, "value": ""});
+    new_keyvaluepair.save(function (err, keyvaluepair) {
+      if (err) {
+        console.log(err);
+        reject(err);
+      };
+    });
+    resolve(new_keyvaluepair);
+  });
+};
+
+async function getallTokens() {
+  return new Promise( function (resolve, reject) {
+    Tokenname.find({}, async function (err, tokenArray) {
+      if (err) {
+        console.log(err);
+        reject(err);
+      };
+      resolve(tokenArray);
+    }).exec();
   });
 };
 
