a9184bc97b734143f0a0141d42e2c5ec0429af27
veraw <Wayne.Vera@sqs.com>
2021-03-22 00:46:04 +0200

    created nodemon.json (ignore bin and temp folders) Implemented Export of transaction

--- api/controllers/DownloadController.js
+++ api/controllers/DownloadController.js
@@ -7,66 +7,176 @@ var fs = require('fs');
 const { execSync } = require("child_process");
 
 var Project = mongoose.model('Project'),
-  Testsuite = mongoose.model('Testsuite');
+  Testsuite = mongoose.model('Testsuite'),
+  Transaction = mongoose.model('Transaction'),
+  Scenario = mongoose.model('Scenario'),
+  Gherkinstep = mongoose.model('Gherkinstep');
+
+const generateTransactionChildren = async function (transaction) {
+  var items = []
+
+  var steps = await Gherkinstep.find({ transaction: transaction._id }).exec();
+
+  for (var step of steps) {
+    items.push({
+      "ID": null,
+      "ParentID": null,
+      "Data": {
+        "TransactionStep": `${step.gherkin_keyword} ${step.name}`,
+        "Expected": "PASS"
+      },
+      "Children": []
+    });
+  }
+  return items;
+}
 
 exports.download_tfs_transaction_by_id = async function (req, res) {
 
-  var file = __dirname + '../../../bin/TSDM.Excel.Exporter.exe';
-  var output = __dirname + '../../../bin/TSDM.xlsx';
-  var jsonFile = __dirname + '../../../bin/TSDM.json';
+  try {
+    var transaction_id = req.params.id;
+    var transaction = await Transaction.findById(transaction_id)
+      .populate('scenario')
+      .populate('gherkinsteps')
+      .exec();
 
-  var projectID = req.query.projectID || "6049f040ada0794850f0deca";
+    var exporterFilePath = path.normalize(__dirname + '../../../bin/TSDM.Excel.Exporter.exe');
+    var outputFilePath = path.normalize(__dirname + `../../../temp/WorkItem_${transaction.alm_id}.xlsx`);
+    var inputFilePath = path.normalize(__dirname + `../../../temp/WorkItem_${transaction.alm_id}.json`);
+    var items = [];
 
-  var test = execSync(`TSDM.Excel.Exporter.exe "Test.json" "TSDM.xlsx"`, { cwd: path.dirname(file) });
-  console.log(test.toString());
+    items.push({
+      "ID": transaction.scenario.alm_id,
+      "ParentID": null,
+      "Data": {
+        "Work Item Type": "User Story",
+        "Title": transaction.scenario.name
+      },
+      "Children": []
+    });
 
-  var outputname = path.basename(output);
-  var outputFile = path.normalize(output);
-  var mimetype = mime.lookup(output);
+    items.push({
+      "ID": transaction.alm_id,
+      "ParentID": transaction.scenario.alm_id,
+      "Data": {
+        "Work Item Type": "Test Case",
+        "Title": transaction.name
+      },
+      "Children": await generateTransactionChildren(transaction)
+    });
 
-  res.setHeader('Content-disposition', 'attachment; filename=' + outputname);
-  res.setHeader('Content-type', mimetype);
+    /* 
+    {
+      "ID": 134,
+      "ParentID": 45,
+      "Data": {
+        "Work Item Type": "Test Case",
+        "Title": "AC 40 - Another Test"
+      },
+      "Children": [
+        {
+          "ID": null,
+          "ParentID": null,
+          "Data": {
+            "TransactionStep": "GIVEN I have navigated to the login page",
+            "Expected": "PASS"
+          },
+          "Children": []
+        },
+        {
+          "ID": null,
+          "ParentID": null,
+          "Data": {
+            "TransactionAction": "Navigate to login page",
+            "Expected": "PASS"
+          },
+          "Children": []
+        },
+        {
+          "ID": null,
+          "ParentID": null,
+          "Data": {
+            "TransactionStep": "WHEN {username} and {password} is entered",
+            "Expected": "PASS"
+          },
+          "Children": []
+        },
+        {
+          "ID": null,
+          "ParentID": null,
+          "Data": {
+            "TransactionAction": "Enter username in username field",
+            "Expected": "PASS"
+          },
+          "Children": []
+        },
+        {
+          "ID": null,
+          "ParentID": null,
+          "Data": {
+            "TransactionAction": "Enter password in password field",
+            "Expected": "PASS"
+          },
+          "Children": []
+        },
+        {
+          "ID": null,
+          "ParentID": null,
+          "Data": {
+            "TransactionStep": "AND Login button in clicked",
+            "Expected": "PASS"
+          },
+          "Children": []
+        },
+        {
+          "ID": null,
+          "ParentID": null,
+          "Data": {
+            "TransactionAction": "Click Login button",
+            "Expected": "PASS"
+          },
+          "Children": []
+        },
+        {
+          "ID": null,
+          "ParentID": null,
+          "Data": {
+            "TransactionStep": "THEN {username} is logged in",
+            "Expected": "PASS"
+          },
+          "Children": []
+        },
+        {
+          "ID": null,
+          "ParentID": null,
+          "Data": {
+            "TransactionAction": "Read username in logged in user label",
+            "Expected": "PASS"
+          },
+          "Children": []
+        }
+      ]
+    },*/
 
-  var filestream = fs.createReadStream(output);
-  filestream.pipe(res);
-
-  // Testsuite.find({ project: projectID }, (err, testSuites) => {
-  //   var items = [];
-
-  //   testSuites.forEach(ts => {
-  //     items.push({
-  //       "ID": ts.alm_id,
-  //       "ParentID": null,
-  //       "Data": {
-  //         "Work Item Type": "User Story",
-  //         "Title": ts.name
-  //       },
-  //       "Children": []
-  //     })
-  //   });
-
-  //   var jsonContent = JSON.stringify(items);
-  //   console.log(jsonContent);
-
-  //   fs.writeFileSync(jsonFile, jsonContent, 'utf8');
+    fs.writeFileSync(inputFilePath, JSON.stringify(items));
 
 
-  //   var test = execSync(`TSDM.Excel.Exporter.exe "TSDM.json" "TSDM.xlsx"`, { cwd: path.dirname(file) });
-  //   console.log(test.toString());
+    var test = execSync(`TSDM.Excel.Exporter.exe "${inputFilePath}" "${outputFilePath}"`, { cwd: path.dirname(exporterFilePath) });
+    console.log(test.toString());
 
-  //   var outputname = path.basename(output);
-  //   var outputFile = path.normalize(output);
-  //   var mimetype = mime.lookup(output);
+    var outputname = path.basename(outputFilePath);
+    var outputFile = path.normalize(outputFilePath);
+    var mimetype = mime.lookup(outputFilePath);
 
-  //   res.setHeader('Content-disposition', 'attachment; filename=' + outputname);
-  //   res.setHeader('Content-type', mimetype);
+    res.setHeader('Content-disposition', 'attachment; filename=' + outputname);
+    res.setHeader('Content-type', mimetype);
 
-  //   var filestream = fs.createReadStream(output);
-  //   filestream.pipe(res);
-  // });
+    var filestream = fs.createReadStream(outputFilePath);
+    filestream.pipe(res);
 
-
-  //res.send(200);
+  } catch (err) {
+    res.status(500).send({ error: err });
+  }
 };
 
 
