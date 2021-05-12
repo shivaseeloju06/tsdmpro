82d709f0f4399fb498f0ebddea7a2fade152b4ff
riaanroos <riaan.roos@expleogroup.com>
2021-05-12 07:51:09 +0200

    added sonar-project.js

---
+++ sonar-project.js
@@ -0,0 +1,21 @@
+const scanner = require("sonarqube-scanner");
+const pkg = require("./package.json");
+
+scanner(
+    {
+        serverUrl:
+            process.env.SONARQUBE_SERVER_URL || "http://swstpbld002.pd.group.intl:9000/",
+        options: {
+            "sonar.projectKey": "TSDM_API",
+            "sonar.projectName": "TSDM_API",
+            "sonar.projectVersion": pkg.version,
+            "sonar.sources": "src"
+            //"sonar.tests": "tests"
+            //"sonar.typescript.lcov.reportPaths": ".test_coverage/lcov.info",
+            //"sonar.testExecutionReportPaths": ".test_output/test-report.xml"
+        }
+    },
+    () => {
+        // callback is required
+    }
+);
